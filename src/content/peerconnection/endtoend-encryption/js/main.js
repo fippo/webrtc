6457f22b7ccfe2e1ed14962fd2d3a2117d93479b
Philipp Hancke <philipp.hancke@googlemail.com>
2020-10-12 13:41:22 +0200

    e2ee: just count things
    counting is hard. This shows the receiver does not get things in the order the
    receiver inserts them.

--- src/content/peerconnection/endtoend-encryption/js/main.js
+++ src/content/peerconnection/endtoend-encryption/js/main.js
@@ -147,12 +147,14 @@ function call() {
   // packets and listens in, but since we don't have
   // access to raw packets, we just send the same video
   // to both places.
+  /*
   startToMiddle = new VideoPipe(localStream, true, false, e => {
     // Do not setup the receiver transform.
     videoMonitor.srcObject = e.streams[0];
   });
   startToMiddle.pc1.getSenders().forEach(setupSenderTransform);
   startToMiddle.negotiate();
+  */
 
   startToEnd = new VideoPipe(localStream, true, true, e => {
     setupReceiverTransform(e.receiver);
--- src/content/peerconnection/endtoend-encryption/js/worker.js
+++ src/content/peerconnection/endtoend-encryption/js/worker.js
@@ -63,70 +63,28 @@ function dump(encodedFrame, direction, max = 16) {
   );
 }
 
-let scount = 0;
+let sendCount = 0;
 function encodeFunction(encodedFrame, controller) {
-  if (scount++ < 30) { // dump the first 30 packets.
-    dump(encodedFrame, 'send');
-  }
-  if (currentCryptoKey) {
-    const view = new DataView(encodedFrame.data);
-    // Any length that is needed can be used for the new buffer.
-    const newData = new ArrayBuffer(encodedFrame.data.byteLength + 5);
-    const newView = new DataView(newData);
+  const view = new DataView(encodedFrame.data);
+  const newData = new ArrayBuffer(encodedFrame.data.byteLength + 4);
+  const newView = new DataView(newData);
 
-    const cryptoOffset = useCryptoOffset? frameTypeToCryptoOffset[encodedFrame.type] : 0;
-    for (let i = 0; i < cryptoOffset && i < encodedFrame.data.byteLength; ++i) {
-      newView.setInt8(i, view.getInt8(i));
-    }
-    // This is a bitwise xor of the key with the payload. This is not strong encryption, just a demo.
-    for (let i = cryptoOffset; i < encodedFrame.data.byteLength; ++i) {
-      const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length);
-      newView.setInt8(i, view.getInt8(i) ^ keyByte);
-    }
-    // Append keyIdentifier.
-    newView.setUint8(encodedFrame.data.byteLength, currentKeyIdentifier % 0xff);
-    // Append checksum
-    newView.setUint32(encodedFrame.data.byteLength + 1, 0xDEADBEEF);
-
-    encodedFrame.data = newData;
+  for (let i = 0; i < encodedFrame.data.byteLength; i++) {
+    newView.setInt8(i, view.getInt8(i));
   }
+  // Set the send counter.
+  newView.setUint32(encodedFrame.data.byteLength, sendCount++);
+
+  encodedFrame.data = newData;
   controller.enqueue(encodedFrame);
 }
 
-let rcount = 0;
 function decodeFunction(encodedFrame, controller) {
-  if (rcount++ < 30) { // dump the first 30 packets
-    dump(encodedFrame, 'recv');
-  }
   const view = new DataView(encodedFrame.data);
-  const checksum = encodedFrame.data.byteLength > 4 ? view.getUint32(encodedFrame.data.byteLength - 4) : false;
-  if (currentCryptoKey) {
-    if (checksum !== 0xDEADBEEF) {
-      console.log('Corrupted frame received, checksum ' +
-                  checksum.toString(16));
-      return; // This can happen when the key is set and there is an unencrypted frame in-flight.
-    }
-    const keyIdentifier = view.getUint8(encodedFrame.data.byteLength - 5);
-    if (keyIdentifier !== currentKeyIdentifier) {
-      console.log(`Key identifier mismatch, got ${keyIdentifier} expected ${currentKeyIdentifier}.`);
-      return;
-    }
+  const recvCount = encodedFrame.data.byteLength > 4 ? view.getUint32(encodedFrame.data.byteLength - 4) : false;
+  console.log('DEC', recvCount, encodedFrame.type);
 
-    const newData = new ArrayBuffer(encodedFrame.data.byteLength - 5);
-    const newView = new DataView(newData);
-    const cryptoOffset = useCryptoOffset? frameTypeToCryptoOffset[encodedFrame.type] : 0;
-
-    for (let i = 0; i < cryptoOffset; ++i) {
-      newView.setInt8(i, view.getInt8(i));
-    }
-    for (let i = cryptoOffset; i < encodedFrame.data.byteLength - 5; ++i) {
-      const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length);
-      newView.setInt8(i, view.getInt8(i) ^ keyByte);
-    }
-    encodedFrame.data = newData;
-  } else if (checksum === 0xDEADBEEF) {
-    return; // encrypted in-flight frame but we already forgot about the key.
-  }
+  encodedFrame.data = encodedFrame.data.slice(0, encodedFrame.data.byteLength - 4);
   controller.enqueue(encodedFrame);
 }
 
